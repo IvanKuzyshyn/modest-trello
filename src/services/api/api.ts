1a3279ab9773054bfeee8383989a90de9f5db21a
IvanKuzyshyn <kuzyshyn.ivan@gmail.com>
2021-01-12 18:46:35 +0200

    Create base api wrapper for local storage

---
+++ src/services/api/api.ts
@@ -0,0 +1,70 @@
+import { findIndex, propEq, adjust } from 'ramda'
+import { Entity } from './api.types'
+
+const getUniqueID = () => new Date().getTime()
+
+const readFromStore = (name: Entity) => {
+  const key = name.toString()
+  const rawData = localStorage.getItem(key)
+
+  try {
+    const data = JSON.parse(rawData || '')
+
+    if (!Array.isArray(data)) {
+      return []
+    }
+
+    return data
+  } catch (e) {
+    console.error(e)
+    return []
+  }
+}
+
+const writeToStore = (name: Entity, data: any) => {
+  const key = name.toString()
+  localStorage.setItem(key, JSON.stringify(data))
+}
+
+export const create = <T>(name: Entity, data: T): T => {
+  const existingItems = readFromStore(name)
+  const newItem = { ...data, id: getUniqueID() }
+
+  writeToStore(name, [...existingItems, newItem])
+  return newItem
+}
+
+export const update = <T>(name: Entity, id: number, data: T): T => {
+  const existingItems = readFromStore(name)
+  const targetItemIndex = findIndex(propEq('id', id), existingItems)
+
+  if (targetItemIndex === -1) {
+    throw new Error('Not Found!')
+  }
+
+  const updatedItem = {
+    ...existingItems[targetItemIndex],
+    ...data,
+    id,
+  }
+
+  writeToStore(
+    name,
+    adjust(targetItemIndex, () => updatedItem, existingItems)
+  )
+
+  return updatedItem
+}
+
+export const remove = <T>(name: Entity, id: number): T[] => {
+  const existingItems = readFromStore(name)
+  // @ts-ignore
+  const updatedItems = existingItems.filter((item: T) => item.id !== id)
+
+  writeToStore(name, updatedItems)
+  return updatedItems
+}
+
+export const readAll = <T>(name: Entity): T[] => {
+  return readFromStore(name)
+}
