b2778d31da3592c5010e3d200d6bd274e9fd7fc9
IvanKuzyshyn <kuzyshyn.ivan@gmail.com>
2021-01-12 23:18:58 +0200

    Refactor column deletion flow

--- src/scenes/board-page/components/board/Board.tsx
+++ src/scenes/board-page/components/board/Board.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect } from 'react'
 
 import './Board.css'
 import {Page} from '../../../shared/components/page/Page'
@@ -7,20 +7,13 @@ import {getAllColumns} from '../../selectors/column'
 import {useAction} from '../../../../context/hooks/use-action'
 import {StateAction} from '../../../../context/types/state'
 import {Column as ColumnComponent} from '../column/Column'
-import {DeleteColumnModal} from '../column/DeleteColumnModal'
 import {DefaultColumn} from '../column/DefaultColumn'
 
 export const Board = () => {
-  const [deletingColumnId, setDeletingColumnId] = useState<number | null>(null)
   const columns = useSelector(getAllColumns)
   const readAllColumns = useAction(StateAction.READ_ALL_COLUMNS)
   const readAllCards = useAction(StateAction.READ_ALL_CARDS)
-  const deleteColumn = useAction(StateAction.DELETE_COLUMN)
   const setColumnManager = useAction(StateAction.SET_COLUMN_MANAGER)
-  const handleDeleteColumn = () => {
-    deleteColumn(deletingColumnId)
-    setDeletingColumnId(null)
-  }
 
   useEffect(() => {
     readAllColumns()
@@ -39,19 +32,10 @@ export const Board = () => {
             id={column.id}
             name={column.label}
             color={column.color}
-            onDelete={() => setDeletingColumnId(column.id)}
-            onEdit={() => setColumnManager({ isShown: true, id: column.id })}
           />
         ))}
         <DefaultColumn onCreateColumn={() => setColumnManager({ id: null, isShown: true })} />
       </section>
-      {Boolean(deletingColumnId) && (
-        <DeleteColumnModal
-          columnId={deletingColumnId}
-          onClose={() => setDeletingColumnId(null)}
-          onDelete={handleDeleteColumn}
-        />
-      )}
     </Page>
   )
 }
--- src/scenes/board-page/components/column/Column.tsx
+++ src/scenes/board-page/components/column/Column.tsx
@@ -8,20 +8,21 @@ import {useAction} from '../../../../context/hooks/use-action'
 import {StateAction} from '../../../../context/types/state'
 import {DeleteCardModal} from '../card/DeleteCardModal'
 import {CreateCardButton} from '../card/CreateCardButton'
+import { DeleteConfirmation } from "./DeleteConfirmation";
 
 interface Props {
   id: number
   name: string
   color: string
-  onDelete: () => void
-  onEdit: () => void
 }
 
-export const Column = ({ id, name, color, onDelete, onEdit }: Props) => {
+export const Column = ({ id, name, color }: Props) => {
+  const [isShownConfirmation, showConfirmation] = useState<boolean>(false)
   const [deletingCardId, setDeletingCardId] = useState<number | null>(null)
   const cards = useSelector((state) => getCardsByColumnId(state, id))
   const deleteCard = useAction(StateAction.DELETE_CARD)
   const setCardManager = useAction(StateAction.SET_CARD_MANAGER)
+  const setColumnManager = useAction(StateAction.SET_COLUMN_MANAGER)
   const handleDeleteCard = () => {
     deleteCard(deletingCardId)
     setDeletingCardId(null)
@@ -32,8 +33,8 @@ export const Column = ({ id, name, color, onDelete, onEdit }: Props) => {
       <div className="title">
         <h4>{name}</h4>
         <div>
-          <button onClick={onEdit}>&#9998;</button>
-          <button onClick={onDelete}>&#10006;</button>
+          <button onClick={() => setColumnManager({ isShown: true, id})}>&#9998;</button>
+          <button onClick={() => showConfirmation(true)}>&#10006;</button>
         </div>
       </div>
       {cards.map((card) => (
@@ -45,6 +46,11 @@ export const Column = ({ id, name, color, onDelete, onEdit }: Props) => {
         />
       ))}
       <CreateCardButton onCreate={() => setCardManager({ id: null, columnId: id, isShown: true })} />
+      <DeleteConfirmation
+          id={id}
+          onClose={() => showConfirmation(false)}
+          isVisible={isShownConfirmation}
+      />
       {Boolean(deletingCardId) && (
         <DeleteCardModal
           onClose={() => setDeletingCardId(null)}
