c8d0f498519f82b412b1c674786469cd7a3d8976
IvanKuzyshyn <kuzyshyn.ivan@gmail.com>
2021-01-12 19:09:52 +0200

    Add modal component

---
+++ src/scenes/shared/components/modal/Modal.tsx
@@ -0,0 +1,41 @@
+import React, { useMemo, useEffect } from 'react'
+import { createPortal } from 'react-dom'
+
+import './Modal.css'
+
+interface Props {
+    title: string,
+    children: React.ReactNode,
+    onClose: () => void,
+}
+
+const root = document.getElementById('portals-root')
+
+const Modal = ({ title, children, onClose }: Props) => {
+    const element = useMemo(() => document.createElement('div'), [])
+    element.classList.add('modal-container')
+
+    useEffect(() => {
+        if (!root) {
+            throw new Error('No root found for modal!')
+        }
+
+        root.appendChild(element)
+        return () => {
+            root.removeChild(element)
+        }
+    }, [element])
+
+    return createPortal(
+        <div className="modal-content">
+            <header className="modal-header">
+                {title}
+                <button onClick={onClose}>&#10006;</button>
+            </header>
+            <section className="modal-body">{children}</section>
+        </div>,
+        element
+    )
+}
+
+export default Modal
