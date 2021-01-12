0672c114aa08dcafd88c389924cf26162df85d60
IvanKuzyshyn <kuzyshyn.ivan@gmail.com>
2021-01-12 19:10:13 +0200

    Add app state types

---
+++ src/context/types/state.ts
@@ -0,0 +1,24 @@
+import { Dispatch } from 'react'
+
+import { Card } from '../../scenes/board-page/types/card'
+import { Column } from '../../scenes/board-page/types/column'
+
+export enum StateAction {
+  CREATE_COLUMN,
+  DELETE_COLUMN,
+  EDIT_COLUMN,
+  READ_ALL_COLUMNS,
+  CREATE_CARD,
+  DELETE_CARD,
+  EDIT_CARD,
+  READ_ALL_CARDS,
+}
+
+export interface IReducerState {
+  cards: Card[]
+  columns: Column[]
+}
+
+export interface IAppState extends IReducerState {
+  dispatch: Dispatch<any>
+}
