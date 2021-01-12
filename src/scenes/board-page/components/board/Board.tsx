b3eae585052e071704e9b1a2d9037747783df56a
IvanKuzyshyn <kuzyshyn.ivan@gmail.com>
2021-01-12 18:39:45 +0200

    Create base board component

---
+++ src/scenes/board-page/components/board/Board.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import './Board.css'
+import { Page } from "../../../shared/components/page/Page";
+
+const Board = () => {
+    return (
+        <Page>
+            <section>
+                <h2>Your Board</h2>
+            </section>
+            <section className="board-columns">
+                board page
+            </section>
+        </Page>
+    )
+}
+
+export default Board
