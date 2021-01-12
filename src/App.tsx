6d9ca0eb09647b83c99df21c830ea78cec5a6838
IvanKuzyshyn <kuzyshyn.ivan@gmail.com>
2021-01-12 23:04:36 +0200

    Update components naming

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Board } from './scenes/board-page/components/board/Board'
 import { AppState } from './context/AppState'
 import { ManageColumnModal } from "./scenes/board-page/components/column/ManageColumnModal";
-import { ManageCardModal } from "./scenes/board-page/components/card/ManageCardModalNew";
+import { ManageCardModal } from "./scenes/board-page/components/card/ManageCardModal";
 
 const App = () => (
   <AppState>
--- src/scenes/board-page/components/card/ManageCardModal.tsx
+++ src/scenes/board-page/components/card/ManageCardModal.tsx
@@ -1,81 +1,83 @@
-import React, { useState, useEffect } from 'react'
+import React, {useEffect, useState} from 'react'
+import {useSelector} from "../../../../context/hooks/use-selector";
+import {getCardById, getManagingCardColumnId, getManagingCardId, isCardManagerShown} from "../../selectors/card";
+import {useAction} from "../../../../context/hooks/use-action";
+import {StateAction} from "../../../../context/types/state";
+import {Modal} from "../../../shared/components/modal/Modal";
+import {ColumnSelector} from "../column/ColumnSelector";
 
-import { ColumnSelector } from '../column/ColumnSelector'
-import { Modal } from '../../../shared/components/modal/Modal'
-import { useSelector } from '../../../../context/hooks/use-selector'
-import { getCardById } from '../../selectors/card'
-import { DraftCard } from '../../types/card'
+export const ManageCardModal = () => {
+    const isShown = useSelector(isCardManagerShown)
+    const id = useSelector(getManagingCardId)
+    const columnId = useSelector(getManagingCardColumnId)
+    const card = useSelector(state => getCardById(state, id))
+    const [name, setName] = useState('')
+    const [column, setColumn] = useState(columnId)
+    const setCardManager = useAction(StateAction.SET_CARD_MANAGER)
+    const createCard = useAction(StateAction.CREATE_CARD)
+    const editCard = useAction(StateAction.EDIT_CARD)
 
-interface Props {
-  onCreate: (data: DraftCard) => void
-  onEdit: (data: DraftCard) => void
-  onClose: () => void
-  parentColumnId: number
-  id: number | null
-}
+    useEffect(() => {
+        if (!column && columnId !== column) {
+            setColumn(columnId)
+        }
+    }, [columnId, column])
 
-export const ManageCardModal = ({
-  onCreate,
-  onEdit,
-  onClose,
-  parentColumnId,
-  id,
-}: Props) => {
-  const [name, setName] = useState('')
-  const [column, setColumn] = useState(parentColumnId)
-  const card = useSelector((state) => getCardById(state, id))
+    useEffect(() => {
+        if (!card) {
+            return
+        }
 
-  useEffect(() => {
-    if (!column && parentColumnId !== column) {
-      setColumn(parentColumnId)
-    }
-  }, [parentColumnId, column])
+        setName(card.name)
+        setColumn(card.columnId)
+    }, [card])
 
-  useEffect(() => {
-    if (!card) {
-      return
-    }
-
-    setName(card.name)
-    setColumn(card.columnId)
-  }, [card])
-
-  const isNew = !card
-  const handleChangeName = (event: React.ChangeEvent<HTMLInputElement>) =>
-    setName(event.target.value)
-  const handleChangeColumn = (event: React.ChangeEvent<HTMLSelectElement>) =>
-    setColumn(Number(event.target.value))
-  const isDisabled = name.trim().length === 0
-  const title = isNew ? 'Create New Card' : 'Edit Card'
-  const submitButtonText = isNew ? 'Create' : 'Update'
-  const submitAction = isNew ? onCreate : onEdit
-  const handleSubmit = () => {
-    if (isDisabled) {
-      return
+    if (!isShown) {
+        return null
     }
 
-    submitAction({ name, columnId: column })
-  }
+    const isNew = !card
+    const handleChangeName = (event: React.ChangeEvent<HTMLInputElement>) =>
+        setName(event.target.value)
+    const handleChangeColumn = (event: React.ChangeEvent<HTMLSelectElement>) =>
+        setColumn(Number(event.target.value))
+    const isDisabled = name.trim().length === 0
+    const title = isNew ? 'Create New Card' : 'Edit Card'
+    const submitButtonText = isNew ? 'Create' : 'Update'
+    const handleClose = () => {
+        setCardManager({ id: null, isShown: false, columnId: null })
+        setName('')
+        setColumn(null)
+    }
+    const handleSubmit = () => {
+        if (isDisabled) {
+            return
+        }
 
-  return (
-    <Modal title={title} onClose={onClose}>
-      <div>
-        <input
-          type="text"
-          value={name}
-          name="column-label"
-          onChange={handleChangeName}
-        />
-        <ColumnSelector value={column} onChange={handleChangeColumn} />
-      </div>
-      <div>
-        <button type="button" onClick={onClose}>
-          Cancel
-        </button>
-        <button type="submit" onClick={handleSubmit} disabled={isDisabled}>
-          {submitButtonText}
-        </button>
-      </div>
-    </Modal>
-  )
+        const data = { name, columnId: column }
+        card ? editCard(card.id, data) : createCard(data)
+        handleClose()
+    }
+
+    return (
+        <Modal title={title} onClose={handleClose}>
+            <div>
+                <input
+                    type="text"
+                    value={name}
+                    name="column-label"
+                    onChange={handleChangeName}
+                />
+                <ColumnSelector value={column || ''} onChange={handleChangeColumn} />
+            </div>
+            <div>
+                <button type="button" onClick={handleClose}>
+                    Cancel
+                </button>
+                <button type="submit" onClick={handleSubmit} disabled={isDisabled}>
+                    {submitButtonText}
+                </button>
+            </div>
+        </Modal>
+    )
 }
