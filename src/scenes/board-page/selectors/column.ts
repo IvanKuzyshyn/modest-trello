ca9d7821dd51e3e0c2bcb68348c0127b6fffee0a
IvanKuzyshyn <kuzyshyn.ivan@gmail.com>
2021-01-12 19:56:46 +0200

    Add columns selectors

---
+++ src/scenes/board-page/selectors/column.ts
@@ -0,0 +1,6 @@
+import {StateAccessor} from "../../../context/types/state";
+import {Column} from "../types/column";
+
+export const getAllColumns: StateAccessor<Column[]> = (state) => state.columns
+export const getColumnById: StateAccessor<Column | undefined> = (state, id: number) =>
+    state.columns.find((column) => column.id === id)
