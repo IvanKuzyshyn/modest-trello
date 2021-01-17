aa4747fe390726154adfac800b6f7104e279a8a0
IvanKuzyshyn <kuzyshyn.ivan@gmail.com>
2021-01-17 19:18:40 +0200

    Fix broken test

--- src/scenes/board-page/components/card/__tests__/Card.test.tsx
+++ src/scenes/board-page/components/card/__tests__/Card.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { render, screen, fireEvent } from '@testing-library/react'
+import { render, screen, fireEvent, waitFor } from '@testing-library/react'
+import { AppContext } from "../../../../../context/AppState";
 
 import { Card } from '../Card'
 
@@ -7,21 +8,59 @@ const getProps = (props = {}) => ({
   name: 'Test name',
   onDelete: jest.fn(),
   onEdit: jest.fn(),
+  id: 1,
   ...props,
 })
 
+const renderWithContext = () => {
+  const dispatch = jest.fn()
+  const props = getProps()
+  const context = {
+    cards: [
+      {
+        id: props.id,
+        name: props.name,
+        columnId: 1,
+      }
+    ],
+    columns: [],
+    columnManager: {
+      id: null,
+      isShown: false,
+    },
+    cardManager: {
+      id: null,
+      columnId: null,
+      isShown: false,
+    },
+    dispatch,
+  }
+
+  render(<AppContext.Provider value={context}><Card {...props} /></AppContext.Provider>)
+  return { props, context }
+}
+
 describe('Card', function () {
+  let modalRoot: HTMLDivElement;
+  beforeEach(() => {
+    modalRoot = document.createElement('div')
+    modalRoot.setAttribute('id', 'portals-root')
+    document.body.appendChild(modalRoot)
+  })
+
+  afterEach(() => {
+    document.body.removeChild(modalRoot)
+  })
+
   test('displays name and shot more button', () => {
-    const props = getProps()
-    render(<Card {...props} />)
+    const { props } = renderWithContext()
 
     expect(screen.queryByText(props.name)).toBeInTheDocument()
     expect(screen.queryByText('More')).toBeInTheDocument()
   })
 
   test('shows and hides controls when clicking on more', () => {
-    const props = getProps()
-    render(<Card {...props} />)
+    const { props } = renderWithContext()
 
     fireEvent.click(screen.getByText('More'))
     expect(screen.queryByTestId('card-edit')).toBeInTheDocument()
@@ -31,9 +70,8 @@ describe('Card', function () {
     expect(screen.queryByTestId('card-delete')).not.toBeInTheDocument()
   })
 
-  test('call onDelete callback', () => {
-    const props = getProps()
-    render(<Card {...props} />)
+  test('call onEdit callback', () => {
+    const { props } = renderWithContext()
 
     fireEvent.click(screen.getByText('More'))
     fireEvent.click(screen.getByTestId('card-edit'))
@@ -41,13 +79,11 @@ describe('Card', function () {
     expect(props.onEdit).toHaveBeenCalledTimes(1)
   })
 
-  test('call onEdit callback', () => {
-    const props = getProps()
-    render(<Card {...props} />)
+  test('call onDelete handler and shows confirmation', () => {
+    const { props } = renderWithContext()
 
     fireEvent.click(screen.getByText('More'))
     fireEvent.click(screen.getByTestId('card-delete'))
-    expect(props.onDelete).toHaveBeenCalled()
-    expect(props.onDelete).toHaveBeenCalledTimes(1)
+    expect(screen.getByText(`Are you sure you want to delete: ${props.name} card?`)).toBeInTheDocument()
   })
 })
